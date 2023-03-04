02c8b4891c41c61cda5f11e247c17f0e891cbb71
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2023-03-04 12:22:39 +0530

    Add files via upload

---
+++ Module5/4-3-JSONVIEW/connect.php
@@ -0,0 +1,12 @@
+<?php
+
+    define('HOST','localhost');
+    define('USER','id2372099_perfectwebservice');
+    define('PASS','prakruti123');
+    define('DB','id2372099_perfectwebservice');
+    
+    $con=mysqli_connect(HOST,USER,PASS,DB);
+    
+    
+
+?>
