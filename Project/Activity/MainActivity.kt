38005264a1aa1f9b06786833abd1989219f86fe8
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2023-04-15 12:53:43 +0530

    Add files via upload

---
+++ Project/Activity/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.finalproject_batch_1130.Activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.finalproject_batch_1130.R
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Timer for Splash Screen
+        Handler().postDelayed(Runnable {
+
+            startActivity(Intent(applicationContext,SliderActivity::class.java))
+
+        },3000)//Timing in Miliseconds
+
+    }
+}
